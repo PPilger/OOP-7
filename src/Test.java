c0fac3f8933b85f501d8ee2f180fd9b8cf67952a
kletzi <kletzi@192.168.0.11>
2012-12-03 22:08:34 +0100

    Angepasst

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,3 @@
-import com.sun.org.apache.bcel.internal.generic.CPInstruction;
-import com.sun.org.apache.bcel.internal.generic.Visitor;
-
 
 public class Test {
 
@@ -10,8 +7,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Autodrom f = new Autodrom(100, 100);
-		FastCarFactory fcf = new FastCarFactory(new CarPosition(new Point(50, 50), 0),new CircleStrategy(5),5);
-		FlexibleCarFactory flcf = new FlexibleCarFactory(new CarPosition(new Point(50, 51), 2), new EightStrategy(), 3);
+		FastCarFactory fcf = new FastCarFactory();
+		FlexibleCarFactory flcf = new FlexibleCarFactory();
 		f.addCar(flcf);
 		f.addCar(fcf);
 		f.start();
