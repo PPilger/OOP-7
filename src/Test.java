254d4ef0b035ed706362564f36ec7f60c7b7275f
kletzi <kletzi@192.168.0.11>
2012-12-03 23:02:20 +0100

    Mit Kollisionen :)

--- src/Test.java
+++ src/Test.java
@@ -8,51 +8,55 @@ public class Test {
 		// TODO Auto-generated method stub
 		Autodrom f = new Autodrom(100, 100);
 		
-		FastCarFactory fcf1 = new FastCarFactory();
-		fcf1.setName("FastCar1");
-		fcf1.setPos(new CarPosition(new Point(10, 10), 1));
-		f.addCar(fcf1);
+		FastCarFactory fcf = new FastCarFactory();
 		
-		FastCarFactory fcf2 = new FastCarFactory();
-		fcf2.setName("FastCar2");
-		fcf2.setPos(new CarPosition(new Point(10, 90), 2));
-		fcf2.setStrategy(new CircleStrategy(2));
-		f.addCar(fcf2);
+		fcf.setName("FastCar1");
+		fcf.setPos(new CarPosition(new Point(10, 10), 1));
+		f.addCar(fcf);
 		
-		FastCarFactory fcf3 = new FastCarFactory();
-		fcf3.setName("FastCar3");
-		fcf3.setPos(new CarPosition(new Point(90, 90), 3));
-		fcf3.setStrategy(new FastRandomStrategy());
-		f.addCar(fcf3);
+		fcf.setName("FastCar2");
+		fcf.setPos(new CarPosition(new Point(10, 12), 2));
+		fcf.setStrategy(new CircleStrategy(2));
+		f.addCar(fcf);
+		
+		fcf.setName("FastCar3");
+		fcf.setPos(new CarPosition(new Point(9, 9), 3));
+		fcf.setStrategy(new FastRandomStrategy());
+		f.addCar(fcf);
 				
-		FlexibleCarFactory flcf1 = new FlexibleCarFactory();
-		flcf1.setName("FlexibleCar1");
-		flcf1.setPos(new CarPosition(new Point(90, 10), 4));
-		f.addCar(flcf1);
+		fcf.setName("FastCar4");
+		fcf.setPos(new CarPosition(new Point(11,11),3));
+		f.addCar(fcf);
 		
-		FlexibleCarFactory flcf2 = new FlexibleCarFactory();
-		flcf2.setName("FlexibleCar2");
-		flcf2.setPos(new CarPosition(new Point(25, 25), 1));
-		flcf2.setStrategy(new EightStrategy(2));
-		f.addCar(flcf2);
+		FlexibleCarFactory flcf = new FlexibleCarFactory();
 		
-		FlexibleCarFactory flcf3 = new FlexibleCarFactory();
-		flcf3.setName("FlexibleCar3");
-		flcf3.setPos(new CarPosition(new Point(25, 75), 2));
-		flcf3.setStrategy(new RectangleStrategy(10, 25));
-		f.addCar(flcf3);
+		flcf.setName("FlexibleCar1");
+		flcf.setPos(new CarPosition(new Point(90, 10), 4));
+		f.addCar(flcf);
 		
-		FlexibleCarFactory flcf4 = new FlexibleCarFactory();
-		flcf4.setName("FlexibleCar4");
-		flcf4.setPos(new CarPosition(new Point(75, 75), 2));
-		flcf4.setStrategy(new CircleStrategy(5));
-		f.addCar(flcf4);
+		flcf.setName("FlexibleCar2");
+		flcf.setPos(new CarPosition(new Point(25, 25), 1));
+		flcf.setStrategy(new EightStrategy(2));
+		f.addCar(flcf);
 		
-		FlexibleCarFactory flcf5 = new FlexibleCarFactory();
-		flcf5.setName("FlexibleCar5");
-		flcf5.setPos(new CarPosition(new Point(75, 25), 3));
-		flcf5.setStrategy(new RectangleStrategy(10, 40));
-		f.addCar(flcf5);
+		flcf.setName("FlexibleCar3");
+		flcf.setPos(new CarPosition(new Point(25, 75), 2));
+		flcf.setStrategy(new RectangleStrategy(10, 25));
+		f.addCar(flcf);
+		
+		flcf.setName("FlexibleCar4");
+		flcf.setPos(new CarPosition(new Point(75, 75), 2));
+		flcf.setStrategy(new CircleStrategy(5));
+		f.addCar(flcf);
+		
+		flcf.setName("FlexibleCar5");
+		flcf.setPos(new CarPosition(new Point(75, 25), 3));
+		flcf.setStrategy(new RectangleStrategy(10, 40));
+		f.addCar(flcf);
+		
+		flcf.setName("FlexibleCar6");
+		flcf.setPos(new CarPosition(new Point(90, 11), 1));
+		f.addCar(flcf);
 		
 		f.start();
 	}
